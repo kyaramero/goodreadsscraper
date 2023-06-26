22685582a60bff5f4e8fe5f6a7ccb5cbe3257f3f
kyaramero <julia.b.andrade@outlook.com>
2023-06-26 17:09:41 -0300

    feature: getting genre books from it shelf's page

---
+++ README.md
@@ -0,0 +1,47 @@
+# Goodreads Scraper 🔎📚
+
+![](https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white)
+![GitHub repo size](https://img.shields.io/github/repo-size/kyaramero/goodreadsscraper?style=for-the-badge)
+
+Aplicação para selecionar e armazenar, em um dataframe, livros de algum gênero escolhido pelo usuário, através do Goodreads — plataforma de catalogação e recomendação.
+
+## 🚀 Como usar
+
+1. Clone esse repositório localmente, em uma pasta.
+
+```bash
+  git clone https://github.com/kyaramero/goodreadsscraper.git
+```
+
+2. Abra o terminal dentro da pasta.
+
+3. Instale as dependências.
+
+```bash
+  pip3 install -r requirements.txt
+```
+
+4. Execute a aplicação.
+
+```bash
+  main.py
+```
+
+5. Siga os próximos passos.
+
+## 🌥️ Status e espaço para melhorias
+
+Foi criado com o objetivo de classificar e ordenar livros visto que, a plataforma não dá a opção de ordenação.
+
+O projeto é bem básico e pode ser rodado com Jupiter Notebook e Colab. Simples porém, com espaço para grandes melhorias. O [Goodreads](https://www.goodreads.com/shelf/show/romance), infelizmente, só permite a paginação de estantes a partir de login. Utilizando [Selenium](https://selenium-python.readthedocs.io), a aplicação, então, pode ser expandida, percorrendo pelas páginas.
+
+Além disso, há a ideia de criar uma interface _user friendly_, onde a interação seria através da web em vez do terminal, contando com exibição dos títulos e também a tão amada ordenação.
+
+## 🔗 Links
+
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://github.com/kyaramero)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/kyaramero)
+
+## ✍🏻 Licença
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import requests
+from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
+import numpy as np
 
+session = HTMLSession()
+session.cookies.set("COOKIE_NAME", "the cookie works", domain="https://www.goodreads.com/")
 
+url = 'https://www.goodreads.com/shelf/show/'
 
-url = 'https://www.goodreads.com/shelf/show/lesbian?page=2'
+genre = input('Type the genre you want to search (you can find the name on goodreads): ')
 
 # Get Rating
 def get_rating(input_text):
@@ -12,58 +17,46 @@ def get_rating(input_text):
     if match:
         avg_rating = float(match.group(1))
         return avg_rating
-
+    
 # Get Release Year
 def get_year(input_text):
     match_published = re.search(r'published (\d{4})', input_text)
     if match_published:
         year_published = int(match_published.group(1))
         return year_published
-
-# Variables
+    
+# Variables    
 titles = []
 authors = []
 ratings = []
 released = []
-pages = []
-
-# def num_pages(url):
-#     num = 25 # After that, pages aren't working, loading
-#     for i in range (1, num + 1):
-#         page = url + str(i)
-#         pages.append(page)
-num = 25
 
 def get_data(url):
-    # for i in range (1, num + 1):
-    r = requests.get(url)
-    print(url)
-    soup = BeautifulSoup(r.text, 'lxml')
+    r = session.get(url + genre)
+    r.cookies
+    soup = BeautifulSoup(r.text, 'html.parser')
     for d in soup.find_all('div', attrs={'class': 'elementList'}):
-        info = d.find('div', attrs={'class': 'left'})
-        if (info is not None): 
+        info = d.find('div', attrs={'class', 'left'})
+        if (info is not None):
             title = info.find('img', alt=True)
-            print(title)
             titles.append(title.get('alt'))
-            author = info.find('a', attrs={'class': 'authorName'})
+            author = info.find('a', attrs={'class', 'authorName'})
             authors.append(author.find('span').text)
-            smallText = info.find('span', attrs={'class': 'smallText'})
+            smallText = info.find('span', attrs={'class', 'smallText'})
             rating = get_rating(smallText.text)
             year = get_year(smallText.text)
             ratings.append(rating)
             released.append(year)
-        # print(titles)
+
     return
 
 get_data(url)
 
+released = np.array(released)
 
-# def main():
-#     num_pages(url)
-#     for page in pages:
-#         get_data(page)
-#     # return titles
+df = pd.DataFrame({'Titulo': titles, 'Autor': authors, 'Nota': ratings, 'Ano de lancamento': released})
 
-# print(main())
-# num_pages(url)
-# print(pages)
+# By rating
+df = df.sort_values(by=['Nota'], ascending=False)
+
+df.to_csv('50LivrosGoodreads.csv')
